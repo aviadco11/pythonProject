6df5a23699d6a3634e2466881fc4a0d159ea6862
aviadc <aviad.co1@gmail.com>
2023-04-10 18:24:06 +0300

    another commit

--- 5.py
+++ 5.py
@@ -1,12 +1,13 @@
 isTrue = False
-c = True
+c = False
 a = 2
 b = 2.5
+aaa = "bbb"
 strOne = "One"
 strThree = "Three"
 if (not c) == False:
     print("c is true")
-if a > b and strOne == "aviel" or b > 1:
+if a > b and strOne == "aviel" or b > 1: # if/else
     print("a is greater then b")
 elif not a == b:
     print("a equals b")
